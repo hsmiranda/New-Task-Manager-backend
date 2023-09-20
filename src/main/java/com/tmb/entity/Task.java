79d2979e64fb98e8e604303ed92a7dd678e407dc
Herberson Miranda <herberson.miranda@outlook.com>
2023-09-20 19:14:18 -0300

    Feature: Create Entity Tasks

---
+++ src/main/java/com/tmb/entity/Task.java
@@ -0,0 +1,29 @@
+package com.tmb.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(generator = "seq_tbl_task", strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "seq_tbl_task", sequenceName = "seq_tbl_task")
+    @Column(name = "pk_long_id")
+    private Long id;
+
+    @Column(name = "tasks_txt_name")
+    private String name;
+
+    @Column(name = "tasks_txt_description")
+    private String description;
+
+    @Column(name = "tasks_dt_datedue")
+    private Date dateDue;
+}
